52bb9196717ff77f7f77a0fe4b6ad46436748eb6
ikawamuk <ikawamuk@student.42.fr>
2026-07-23 21:29:44 +0900

    Aria breath !

--- src/AliceModel.ts
+++ src/AliceModel.ts
@@ -1,9 +1,17 @@
+import { CubismDefaultParameterId } from '@framework/cubismdefaultparameterid';
 import { CubismModelSettingJson } from '@framework/cubismmodelsettingjson';
 import { ICubismModelSetting } from '@framework/icubismmodelsetting';
 
 import { CubismUserModel } from '@framework/model/cubismusermodel';
 import { CubismPhysicsUpdater } from '@framework/motion/cubismphysicsupdater';
 import { CubismUpdateScheduler } from '@framework/motion/cubismupdatescheduler';
+
+import type { CubismIdHandle } from '@framework/id/cubismid';
+import {
+  BreathParameterData,
+  CubismBreath
+} from '@framework/effect/cubismbreath';
+import { CubismBreathUpdater } from '@framework/motion/cubismbreathupdater';
 
 import { CubismEyeBlink } from '@framework/effect/cubismeyeblink';
 import { CubismEyeBlinkUpdater } from '@framework/motion/cubismeyeblinkupdater';
@@ -16,6 +24,7 @@ import { AliceGLManager } from './AliceGLManager';
 import { AlicePlatform } from './AlicePlatform';
 import * as AliceDefine from './AliceDefine';
 import { AliceTextureManager } from './AliceTextureManager';
+import { CubismFramework } from '@framework/live2dcubismframework';
 
 
 export interface GraphicsContext {
@@ -27,6 +36,7 @@ export interface GraphicsContext {
 
 export class AliceModel extends CubismUserModel implements Model {
 	public update(): void {
+		if (this._model == null) { return ; }
 		const deltaTimeSeconds: number = AlicePlatform.getDeltaTime();
 		this.userTimeSeconds_ += deltaTimeSeconds;
 		this._model.loadParameters();
@@ -68,8 +78,9 @@ export class AliceModel extends CubismUserModel implements Model {
 			const setting = new CubismModelSettingJson(arrayBuffer, arrayBuffer.byteLength);
 			await this.setupModel(setting, graphicsContext.getCanvas(), graphicsContext.getGLManager());
 			await this.setupPhysics();
-			await this.setupTexture(graphicsContext.getTextureManager());
 			this.setupEyeBlink();
+			this.setupBreath();
+			await this.setupTexture(graphicsContext.getTextureManager());
 		} catch (error) {
 			AlicePlatform.printError('Failed to load Assets', error as Error);
 		}
@@ -82,6 +93,19 @@ export class AliceModel extends CubismUserModel implements Model {
 		this.motionUpdated_ = false;
 		this.updateScheduler_ = new CubismUpdateScheduler();
 		this.modelHomeDir_ = null;
+
+		this.idParamAngleX_ = CubismFramework.getIdManager().getId(
+			CubismDefaultParameterId.ParamAngleX
+		);
+		this.idParamAngleY_ = CubismFramework.getIdManager().getId(
+			CubismDefaultParameterId.ParamAngleY
+		);
+		this.idParamAngleZ_ = CubismFramework.getIdManager().getId(
+			CubismDefaultParameterId.ParamAngleZ
+		);
+		this.idParamBodyAngleX_ = CubismFramework.getIdManager().getId(
+			CubismDefaultParameterId.ParamBodyAngleX
+		);
 	}
 
 	public release(): void {
@@ -136,6 +160,23 @@ export class AliceModel extends CubismUserModel implements Model {
 		this.updateScheduler_.addUpdatableList(eyeBlinkUpdater);
 	}
 
+	private setupBreath(): void {
+		this._breath = CubismBreath.create();
+		// 以下要調査
+		const breathParameters: Array<BreathParameterData> = [
+			new BreathParameterData(this.idParamAngleX_, 0.0, 15.0, 6.5345, 0.5),
+			new BreathParameterData(this.idParamAngleY_, 0.0, 8.0, 3.5345, 0.5),
+			new BreathParameterData(this.idParamAngleZ_, 0.0, 10.0, 5.5345, 0.5),
+			new BreathParameterData(this.idParamBodyAngleX_, 0.0, 4.0, 15.5345, 0.5),
+			new BreathParameterData(
+				CubismFramework.getIdManager().getId(CubismDefaultParameterId.ParamBreath),
+				0.5, 0.5, 3.2345, 1)
+		];
+		this._breath.setParameters(breathParameters);
+		const breathUpdater = new CubismBreathUpdater(this._breath);
+		this.updateScheduler_.addUpdatableList(breathUpdater);
+	};
+
 	private async setupTexture(textureManager: AliceTextureManager): Promise<void> {
 		if (this.modelSetting_ == null) { return ; }
 		const textureCount: number = this.modelSetting_.getTextureCount();
@@ -159,6 +200,11 @@ export class AliceModel extends CubismUserModel implements Model {
 	private userTimeSeconds_: number;
 
 	private motionUpdated_: boolean;
+
+	private idParamAngleX_: CubismIdHandle;
+	private idParamAngleY_: CubismIdHandle;
+	private idParamAngleZ_: CubismIdHandle;
+	private idParamBodyAngleX_: CubismIdHandle;
 
 	private updateScheduler_: CubismUpdateScheduler;
 	private modelSetting_: ICubismModelSetting | null;
